b205c7f0e6f1c4b015741367bb678afde0de98c7
Christian Paul <info@jaller.de>
2021-08-22 16:24:47 +0200

    Place tanks in a grid

--- index.js
+++ index.js
@@ -65,17 +65,21 @@ const tankAIDriver = (tank, desiredState) => {
         turnRight: 0,
         turnTowerRight: 0,
     };
-    if (tank.x.value < desiredState.x.value) {
-        inputs.forward = Math.sin(deg2rad(90 + tank.rotation.value % 360));
-    } else if (tank.x.value > desiredState.x.value) {
-        inputs.forward = -Math.sin(deg2rad(90 + tank.rotation.value % 360));
-    }
-    if (tank.y.value < desiredState.y.value) {
-        inputs.forward += -Math.cos(deg2rad(90 + tank.rotation.value % 360));
-    } else if (tank.y.value > desiredState.y.value) {
-        inputs.forward += Math.cos(deg2rad(90 + tank.rotation.value % 360));
+    // Do we need to drive to a different location?
+    if (!withinRange(tank.x.value, desiredState.x.value, 3) || !withinRange(tank.y.value, desiredState.y.value, 3)) {
+        if (tank.x.value < desiredState.x.value) {
+            inputs.forward = Math.sin(deg2rad(90 + tank.rotation.value % 360));
+        } else if (tank.x.value > desiredState.x.value) {
+            inputs.forward = -Math.sin(deg2rad(90 + tank.rotation.value % 360));
+        }
+        if (tank.y.value < desiredState.y.value) {
+            inputs.forward += -Math.cos(deg2rad(90 + tank.rotation.value % 360));
+        } else if (tank.y.value > desiredState.y.value) {
+            inputs.forward += Math.cos(deg2rad(90 + tank.rotation.value % 360));
+        }
     }
     inputs.forward = limit(inputs.forward, -1, 1);
+    // Do we need to rotate?
     const rotationDiff = ((tank.rotation.value - desiredState.rotation.value) % 360 + 360) % 360;
     if (rotationDiff > 180) {
         inputs.turnRight = 1;
@@ -94,9 +98,10 @@ const tankAIDriver = (tank, desiredState) => {
         inputs.turnRight = -1;
     }
     if (withinRange(inputs.forward, 0, 0.01) && !withinRange(tank.x.value, desiredState.x.value, 25) && !withinRange(tank.y.value, desiredState.y.value, 25)) {
-        // X-Position stimmt, aber Y-Position nicht
+        // Langsame Geschwindigkeit, aber wir sind nicht da
         inputs.turnRight = 1;
     }
+    // Do we need to rotate the tower?
     const towerRotationDiff = ((tank.towerRotation.value - desiredState.towerRotation.value) % 360 + 360) % 360;
     if (towerRotationDiff > 180) {
         inputs.turnTowerRight = 1;
@@ -152,26 +157,63 @@ function stepTank(tank, tankInputs, delta) {
 
 const tanks = [];
 
-for (let i = 0; i < 4; i++) {
-    const state = {
-        color: `#${randomItem('5,7,9,a,c,d,d,e,f'.split(','))}${randomItem('5,7,9,a,c,d,d,e,f'.split(','))}${randomItem('5,7,9,a,c,d,d,e,f'.split(','))}`,
-        x: new Acceleratable(randomInt(0, 1500)),
-        y: new Acceleratable(randomInt(0, 700)),
-        rotation: new Acceleratable(0),
-        towerRotation: new Acceleratable(0),
-    };
-    const desiredState = {
-        ...state,
-        x: new Acceleratable(randomInt(0, 1500)),
-        y: new Acceleratable(randomInt(0, 700)),
-        rotation: new Acceleratable(randomInt(0, 359)),
-        towerRotation: new Acceleratable(randomInt(0, 359)),
-    };
-    tanks.push({
-        state,
-        desiredState,   
-    });
+// Bots get placed in a grid
+
+const screenWidth = Number.parseInt(canvas.width);
+const screenHeight = Number.parseInt(canvas.height);
+for (let x = 0; x < 10; x++) {
+    for (let y = 0; y < 8; y++) {
+        const state = {
+            color: `#${randomItem('5,7,9,a,c,d,d,e,f'.split(','))}${randomItem('5,7,9,a,c,d,d,e,f'.split(','))}${randomItem('5,7,9,a,c,d,d,e,f'.split(','))}`,
+            x: new Acceleratable(randomInt(0, screenWidth)),
+            y: new Acceleratable(randomInt(0, screenHeight)),
+            rotation: new Acceleratable(randomInt(0, 360)),
+            towerRotation: new Acceleratable(randomInt(0, 360)),
+        };
+        const desiredState = {
+            ...state,
+            x: new Acceleratable(x * screenWidth / 10 + 32),
+            y: new Acceleratable(y * screenHeight / 8 + 32),
+            rotation: new Acceleratable(-90),
+            towerRotation: new Acceleratable(0),
+        };
+        const color = `#${'5,7,9,a,c,d,d,e,f'.split(',')[Math.floor(desiredState.x.value / (screenWidth / 8))]}${'6,7,8,9,b,e,b,8,6'.split(',')[Math.floor(desiredState.y.value / (screenHeight / 9))]}${'5,6,7,8,9,a,b,c,d'.split(',')[Math.floor(desiredState.y.value / (screenHeight / 9))]}`;
+        state.color = color;
+        desiredState.color = color;
+        tanks.push({
+            state,
+            desiredState,
+        });
+    }
 }
+
+// Randomly placed bots
+
+// const screenWidth = Number.parseInt(canvas.width);
+// const screenHeight = Number.parseInt(canvas.height);
+// for (let i = 0; i < 200; i++) {
+//     const state = {
+//         color: `#${randomItem('5,7,9,a,c,d,d,e,f'.split(','))}${randomItem('5,7,9,a,c,d,d,e,f'.split(','))}${randomItem('5,7,9,a,c,d,d,e,f'.split(','))}`,
+//         x: new Acceleratable(randomInt(0, screenWidth)),
+//         y: new Acceleratable(randomInt(0, screenHeight)),
+//         rotation: new Acceleratable(0),
+//         towerRotation: new Acceleratable(0),
+//     };
+//     const desiredState = {
+//         ...state,
+//         x: new Acceleratable(randomInt(0, screenWidth)),
+//         y: new Acceleratable(randomInt(0, screenWidth)),
+//         rotation: new Acceleratable(randomInt(0, 359)),
+//         towerRotation: new Acceleratable(randomInt(0, 359)),
+//     };
+//     const color = `#${'5,7,9,a,c,d,d,e,f'.split(',')[Math.floor(desiredState.x.value / (screenWidth / 8))]}${'6,7,8,9,b,e,b,8,6'.split(',')[Math.floor(desiredState.y.value / (screenHeight / 9))]}${'5,6,7,8,9,a,b,c,d'.split(',')[Math.floor(desiredState.y.value / (screenHeight / 9))]}`;
+//     state.color = color;
+//     desiredState.color = color;
+//     tanks.push({
+//         state,
+//         desiredState,
+//     });
+// }
 
 function step(timestamp) {
     if (start === undefined) {
@@ -189,11 +231,11 @@ function step(timestamp) {
         const inputs = tankAIDriver(tank.state, tank.desiredState);
         stepTank(tank.state, inputs, delta);
         drawTank(tank.state);
-        drawTank(tank.desiredState);
+        // drawTank(tank.desiredState);
     }
 
-    stepTank(playerTank, tankInputs, delta);
-    drawTank(playerTank);
+    // stepTank(playerTank, tankInputs, delta);
+    // drawTank(playerTank);
     
     // ctx.fillStyle = 'white';
     // ctx.fillText(`${elapsed.toFixed(0)} ${previousTimeStamp.toFixed(0)}`, 0, 10);
